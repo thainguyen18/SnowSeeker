ef59ebbeb97c9493c3fb7ebd4fb4c0eca7381dfa
thainguyen18 <42723566+thainguyen18@users.noreply.github.com>
2020-01-11 23:05:24 -0600

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Thai Nguyen on 1/11/20.
+//  Copyright © 2020 Thai Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
